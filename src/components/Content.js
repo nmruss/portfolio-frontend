06029e2405185d590886aadf11268ef5d8db7302
nick russo <nick.russo@bbdo.com>
2019-12-06 18:10:56 -0500

    content container, content card components

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import ContentCard from "./ContentCard";
+const projectData = require("../data/project_data.json");
 
 const Content = () => {
+  function renderContent() {
+    let contentCards = [];
+    projectData.forEach((project, ind, arr) => {
+      const { title, thumbnail, role, type } = project;
+      contentCards.push(
+        <Row>
+          <Col md="2"></Col>
+          <Col md="8">
+            <ContentCard title={title} thumbnail={thumbnail} role={role} type={type} />
+          </Col>
+          <Col md="2"></Col>
+        </Row>
+      );
+    });
+    return contentCards;
+  }
   return (
     <Container fluid className="content-container">
       <Row>
@@ -9,7 +27,7 @@ const Content = () => {
         <Col sm="8" md="8">
           <div className="content-header-container">
             <div>
-              <h1>i'm nick</h1>
+              <h1 className="black">i'm nick</h1>
             </div>
             <div>
               <h1>
@@ -25,9 +43,9 @@ const Content = () => {
             </div>
           </div>
         </Col>
-
         <Col sm="2" md="2"></Col>
       </Row>
+      {renderContent()}
     </Container>
   );
 };
---
+++ src/components/ContentCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+const ContentCard = ({ title, thumbnail, role, summary }) => {
+  return (
+    <Container fluid className="content-card-container">
+      <Row>
+        <Col md="6">
+          <img src={thumbnail} alt={"thumbnail-" + title}></img>
+        </Col>
+        <Col md="1"></Col>
+        <Col md="5">
+          <div className="content-text-container">
+            <h1 className="content-title black">{title}</h1>
+            <h3 className="content-title">{"Role:" + role}</h3>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default ContentCard;
